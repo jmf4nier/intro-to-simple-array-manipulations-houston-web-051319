a7df73bb50053a810f326588b374366b11d6db67
Jason Fournier <jmf4nier@hotmail.com>
2019-05-01 18:22:28 +0000

    Automatically backed up by Learn

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,47 @@
+def using_push(array, string)
+  array.push(string)
+end
+
+def using_unshift(array, string)
+  array.unshift(string)
+end
+
+def using_pop(array)
+  array.pop
+end
+
+def pop_with_args(array)
+  array.pop(2)
+end
+
+def using_shift(array) 
+
+end
+
+def shift_with_args(array) 
+  
+end
+
+def using_concat(array, array)
+  
+end
+
+def using_insert(array, element) 
+  
+end
+
+def using_uniq(array)
+  
+end
+
+def using_flatten(array) 
+  
+end
+
+def using_delete(array, string)
+  
+end
+
+def using_delete_at(array, index)
+  
+end
